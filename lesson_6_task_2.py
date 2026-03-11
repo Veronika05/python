cfbb0e6427e96b37473c5e3482a858bab03b9bec
Вероника <efremovaveronika053@gmail.com>
2026-03-11 12:49:22 +0300

    Добавлены изменения в файлы

--- lesson_6_task_2.py
+++ lesson_6_task_2.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
@@ -12,6 +13,11 @@ button_name = driver.find_element(By.CSS_SELECTOR, "#newButtonName")
 button_name.send_keys("SkyPro")
 
 button = driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+
+waiter = WebDriverWait(driver, 20)
+waiter.until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#updatingButton"), "SkyPro")
+)
 
 print(driver.find_element(By.CSS_SELECTOR, ".btn-primary").text)
 
